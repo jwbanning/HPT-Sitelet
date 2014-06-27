4f6b599dc408fe2fd07f9ab4e2091fcd8ff4d9a7
Vincent W Profeta <vincent.profeta@mutualmobile.com>
2014-06-27 15:05:21 -0500

    refactoring and editing JOHNS getting started page - john we need to talk ...............about you being a BITCH

--- Responsive/GetStarted/js/getting-started.js
+++ Responsive/GetStarted/js/getting-started.js
@@ -1,13 +1,27 @@
 
 $(document).ready(function() {
-
     $('form input[name=brokerage]').change(function(){
-      $('.commisionFreeContainer p').removeClass('hide').addClass('show');
-      $('#moreInfo').html('- Less Info');
+      // $('.commisionFreeContainer p').removeClass('hide').addClass('show');
+      // $('#moreInfo').html('- Less Info');
+    });
+
+    $('.formContainer form input').change(function(e){
+      if (!$(e.currentTarget).siblings('.commisionFreeContainer').hasClass('active')) {
+        $(e.currentTarget).siblings('.commisionFreeContainer').addClass('active');
+      };
+    });
+
+    $('.seven60ContentRight .copy2 input').change(function(e){
+      if ($(e.currentTarget).is(':checked') == true ) {
+        $('.fund-earn').addClass('active');
+      }
+      else {
+        $('.fund-earn').removeClass('active');
+      }
     });
 
     $( "#moreInfo" ).click(function() {
-      if ($( ".commisionFreeContainer p" ).hasClass('hide')){
+      if ($(".commisionFreeContainer p" ).hasClass('hide')){
         $('.commisionFreeContainer p').removeClass('hide').addClass('show');
         $('#moreInfo').html('- Less Info');
       }else{
