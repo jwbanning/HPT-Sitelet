e79875d923fc5d4953dd119e58a52f38ec864a58
Vincent W Profeta <vincent.profeta@mutualmobile.com>
2014-06-27 09:57:30 -0500

    home page edits

--- Responsive/Home/js/home.js
+++ Responsive/Home/js/home.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
     $('.box-wrapper .box h2').removeClass('active');
     $(this).addClass('active');
   })
+
+  $('.box.selector h2').click(function(e) {
+    var id = $(e.currentTarget).data('id');
+    $('.selected p').removeClass('active');
+    $(e.currentTarget).parent().siblings('.selected').find("[data-id='" + id + "']").addClass('active');
+  })
+
+   $('.dollar-amount h4').click(function(e) {
+      $(e.currentTarget).closest('.mobile-pricing .column').toggleClass('active');
+   });
+
 });
